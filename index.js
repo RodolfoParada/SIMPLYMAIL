9a4efa1c7fb49a440f96804acf8e01dab0a889c3
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2023-05-10 00:27:14 -0400

    vistaProyecto

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var input = document.getElementById("asunto");
+    input.value = "Para:";
+
+    var input = document.getElementById("resumenPedido");
+    input.value = "RESUMEN DEL PEDIDO";
+};
